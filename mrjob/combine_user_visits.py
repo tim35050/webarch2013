bf12ea94dcfb3f26a0a5a8fdc7e4f2628d0bd385
Tim <tmeyers@ischool.berkeley.edu>
2013-12-02 18:25:39 -0800

    hw11 submission

--- mrjob/combine_user_visits.py
+++ mrjob/combine_user_visits.py
@@ -30,17 +30,9 @@ def main():
     for line in fileinput.input():
         cell = csv_readline(line)
         if cell[0] == 'C':
-            ###
-            # FILL IN by replacing below:
-            pass
-            # What should we update when we see a new 'C' row?
-            ##/
+            current_user = cell[2]
         elif cell[0] == 'V':
-            ###
-            # FILL IN by replacing below:
-            pass
-            # What should we update when we see a new 'V' row?
-            ##/
+            cell[2] = current_user
 
         csv_writer.writerow(cell)
 
